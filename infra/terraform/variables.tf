532b97ba2e5bf6c5c8f12b94975e705d9332b907
Dav <davit.grigoryan22@gmail.com>
2025-10-07 21:10:08 +0400

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project"        { type = string }
-variable "aws_region"     { type = string  default = "eu-central-1" }
+variable "aws_region"     { type = string  default = "eu-north-1" }
 variable "instance_type"  { type = string  default = "t3.micro" }
-variable "key_name"       { type = string }         # EC2 Key Pair անուն
-variable "vpc_id"         { type = string }         # եթե թափանցիկ VPC չունես, կարող եմ ավելացնել VPC ստեղծում
-variable "public_subnet_id" { type = string }       # public subnet ID
-variable "allow_ssh_cidr" { type = string  default = "0.0.0.0/0" }
+variable "key_name"       { type = testBDG }         # EC2 Key Pair անուն
+variable "vpc_id"         { type = vpc-0cd0ec95473967e71 }         # եթե թափանցիկ VPC չունես, կարող եմ ավելացնել VPC ստեղծում
+variable "public_subnet_id" { type = subnet-01b588c348b24bdeb}       # public subnet ID
+variable "allow_ssh_cidr" { type = string  default = "13.62.128.212" }
